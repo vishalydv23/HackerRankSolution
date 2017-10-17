15f31ad6b9f596b7fdc89ccb00d2aacc7f34b19b
Vishal Yadav <vishalydv23@gmail.com>
2017-10-17 20:31:03 +0100

    comment added

--- cracking the coding interview/1_left_array_rotation.py
+++ cracking the coding interview/1_left_array_rotation.py
@@ -1,3 +1,11 @@
+# A Juggling Algorith
+
+# Instead of moving one by one, divide the array in different sets
+# where number of sets is equal to GCD of n and d and move the elements within sets.
+# If GCD is 1 as is for the above example array (n = 7 and d =2), then elements will be 
+# moved within one set only, we just start with temp = arr[0] and keep moving arr[I+d] to arr[I] 
+# and finally store temp at the right place.
+
 def gcdfunction(a, b):
     if(b == 0):
         return a
